40efd1c659dc4dc98a05d1454c91a10261891d8a
chris <chrszhu@yahoo.com>
2018-10-09 18:33:07 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send({
+    errorMessage: 'unable to handle request',
+    hello: ['wtf', 'wtfs']
+
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.send('About page');
+});
+
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'unable to handle request'
+  })
+});
+app.listen(3000);
