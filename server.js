d0ec1de86bc2c36daa8d331b1cb5fb5dee9a2033
chris <chrszhu@yahoo.com>
2018-10-16 19:42:30 -0700

    help

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = re  quire('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -21,9 +23,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-})
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// })
 
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -53,4 +55,6 @@ app.get('/bad', (req, res) => {
     errorMessage: 'unable to handle request'
   })
 });
-app.listen(3000);
+app.listen(3000, () => {
+    console.log(`server is up on port ${port}`);
+});
